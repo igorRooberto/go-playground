bce42262c9ee69a852a28c3e06a17490092f630b
Igor <isilvalopp@gmail.com>
2026-09-04 14:49:13 -0300

    Exercicio 1/ FOR

---
+++ calculadora/for/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 10; i++ {
+
+		for j := 1; j <= 10; j++ {
+			fmt.Println(i, " X ", j, " = ", i*j)
+		}
+
+		fmt.Println()
+
+	}
+
+}
